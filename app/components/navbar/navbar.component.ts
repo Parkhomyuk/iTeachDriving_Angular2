220837c4dba2a2bbddeacb3c390b4cb52457e257
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2016-12-03 15:34:05 +0200

    Initial commit

---
+++ app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+
+  selector: 'navbar',
+  styleUrls:['../../../css/style.css'],
+  templateUrl: 'navbar.component.html'
+})
+export class NavbarComponent { }
